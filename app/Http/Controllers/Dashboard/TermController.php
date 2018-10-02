d78ba1c476fa13b1774ddfe034f6f3ad702603fe
maradney <maradney.dev@gmail.com>
2018-10-02 12:40:46 +0200

    [dashboard] modifying terms & conditions page

--- app/Http/Controllers/Dashboard/TermController.php
+++ app/Http/Controllers/Dashboard/TermController.php
@@ -5,90 +5,104 @@ namespace App\Http\Controllers\Dashboard;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Term;
+use App\TermFAQ;
 
 class TermController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    * Display a listing of the resource.
+    *
+    * @return \Illuminate\Http\Response
+    */
     public function index()
     {
-        $term = Term::first();
-        return view('dashboardV2.terms.index')->with('term',$term);
+        $data['term'] = Term::first();
+        $data['terms_faq'] = TermFAQ::all();
+
+        return view('dashboardV2.terms.index' ,$data);
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    * Show the form for creating a new resource.
+    *
+    * @return \Illuminate\Http\Response
+    */
     public function create()
     {
         return view('dashboardV2.terms.create');
     }
 
     /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+    * Store a newly created resource in storage.
+    *
+    * @param  \Illuminate\Http\Request  $request
+    * @return \Illuminate\Http\Response
+    */
     public function store(Request $request)
     {
         $delete = Term::where('id','!=',null)->delete();
         $term = Term::create([
-            'description'=>$request->input('body')
+            'description' => $request->input('body')
         ]);
+
+        if ($request->get('faq')) {
+            TermFAQ::where('id','!=',null)->delete();
+            TermFAQ::insert($request->get('faq'));
+        }
 
         return redirect()->route('dashboard.terms.index');
     }
 
     /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    * Display the specified resource.
+    *
+    * @param  int  $id
+    * @return \Illuminate\Http\Response
+    */
     public function show($id)
     {
         //
     }
 
     /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    * Show the form for editing the specified resource.
+    *
+    * @param  int  $id
+    * @return \Illuminate\Http\Response
+    */
     public function edit($id)
     {
-        $term = Term::where('id',$id)->first();
-        return view('dashboardV2.terms.edit')->with('term',$term);
+        $data['term'] = Term::first();
+        $data['terms_faq'] = TermFAQ::all();
+
+        return view('dashboardV2.terms.edit' ,$data);
     }
 
     /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    * Update the specified resource in storage.
+    *
+    * @param  \Illuminate\Http\Request  $request
+    * @param  int  $id
+    * @return \Illuminate\Http\Response
+    */
     public function update(Request $request, $id)
     {
         $updateTerm = Term::where('id',$id)->update([
             'description'=>$request->input('body')
         ]);
-         return redirect()->route('dashboard.terms.index');
+        if ($request->get('faq')) {
+            TermFAQ::where('id','!=',null)->delete();
+            TermFAQ::insert($request->get('faq'));
+        }
+        return redirect()->route('dashboard.terms.index');
     }
 
     /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    * Remove the specified resource from storage.
+    *
+    * @param  int  $id
+    * @return \Illuminate\Http\Response
+    */
     public function destroy($id)
     {
         //
