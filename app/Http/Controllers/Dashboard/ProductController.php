7d2c87682a5ce755a5509c19ea46a2455ecf8e94
maradney <maradney.dev@gmail.com>
2018-10-30 12:40:58 +0200

    fixing colors ,sizes in product

--- app/Http/Controllers/Dashboard/ProductController.php
+++ app/Http/Controllers/Dashboard/ProductController.php
@@ -315,6 +315,7 @@ class ProductController extends BaseController
     {
         $data = $request->all();
         if (isset($data['size'])) {
+            dd($data['size']);
             $data['size'] = implode(',', $data['size']);
         }
         if (isset($data['color'])) {
